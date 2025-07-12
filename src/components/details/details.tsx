e4c3a07188d6683259811ef45e6dc9f246b75775
Rakotonirina Safidy <Safidyrktnr@gmail.com>
2025-07-12 03:26:18 +0300

    Update details.tsx

--- src/components/details/details.tsx
+++ src/components/details/details.tsx
@@ -7,14 +7,14 @@ export const Details = () => {
     <>
     <div className='text-black flex items-center justify-center p-20'>
       <div className='w-1/2 items-center justify-center flex'>
-        <img src="public/images/pdp.png" alt="" className=' bg-gray-300 rounded-full w-2/3'/>
+        <img src="/images/pdp.png" alt="" className=' bg-gray-300 rounded-full w-2/3'/>
       </div>
       <div className='w-1/2 text-justify p-20'>
         <div>
           <p>Bonjour,</p>
           <p>je suis <span><span>Safidy</span>Rakotonirina</span>,</p>
           <p><span>developpeur front-end</span>passionné.</p>
-          <p>Je maîtrise plusieurs langages de programmation, dont <span>HTML</span>, <span >CSS</span>, <span>JavaScript</span> et <span>PHP</span>.</p>
+          <p>Je maîtrise plusieurs langages de programmation, dont <span>HTML</span>, <span >CSS</span>, <span>JavaScript</span>, <span>ReactJs</span> et <span>PHP</span>.</p>
           <p>Toujours curieux et motivé, j’aime découvrir de nouvelles technologies et m’investir pleinement dans le plaisir d’apprendre et de les maîtriser.</p>
         </div>
         <div>
